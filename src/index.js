d60987e5410812f8181afe53b13eb270d7693ada
Luka Casey <lukacasaletto2000@gmail.com>
2023-04-27 20:48:13 +1000

    Fix bug

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
